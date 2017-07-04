593d39b8eef2a4df819b6edbec6df1d168dc15ab
Joel Urraco <joel.urraco@clouddistrict.com>
2017-07-04 08:39:31 +0200

    Gitignore

--- params.sh
+++
@@ -1,7 +0,0 @@
-export TODO_STORE_HOST=localhost
-export TODO_STORE_PORT=5432
-export TODO_STORE_NAME=tododb
-export TODO_STORE_USER=todouser
-export TODO_STORE_PASS=password
-export TODO_MODE=dev
-export TODO_PORT=29400
